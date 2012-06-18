f2d34b63bb6d2c24ecfaf267833085f0d95a72f5
John Borges <jab24@njit.edu>
2012-06-17 21:54:05 -0400

    added icon files and made minor revisions

--- javascripts/basic.js
+++ javascripts/basic.js
@@ -1,4 +1,7 @@
 // Add a test to Modernirz for standalone mode
-  Modernizr.addTest('standalone',function(){
-    return window.navigator.standalone;
-  });
+Modernizr.addTest('standalone',function(){
+	return window.navigator.standalone;
+});
+if(!!navigator.standalone){
+	document.documentElement.setAttribute('class,document.documentElement.getAttribute('class') + "standalone");
+}
